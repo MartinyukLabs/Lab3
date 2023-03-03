36b7ccd36014e3429562a4629db24fa75ebc3ee4
MartinyukLabs <I.Martynyuk_FIT_2_21_B_d@knute.edu.ua>
2023-03-03 14:03:36 +0200

    Finish is here

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -11,7 +11,7 @@ class Program
 {
     static void Main()
     {
-        Third();
+        Fourth();
     }
     /////////////////////////////////////////1
     public static void First()
@@ -234,6 +234,40 @@ class Program
     /////////////////////////////////////////4
     public static void Fourth()
     {
+        string folderPath = "files";
 
+        Func<string, IEnumerable<string>> tokenizeFile = filePath =>
+        {
+            string text = File.ReadAllText(filePath);
+            return text.Split(new[] { ' ', '.', ',', '!', '?', ';', ':', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        };
+
+        Func<IEnumerable<string>, IDictionary<string, int>> countWords = words =>
+        {
+            var wordCounts = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            foreach (var word in words)
+                if (wordCounts.ContainsKey(word))
+                    wordCounts[word]++;
+                else
+                    wordCounts[word] = 1;
+            return wordCounts;
+        };
+
+        Action<IDictionary<string, int>> displayWordCounts = wordCounts =>
+        {
+            foreach (var pair in wordCounts.OrderByDescending(pair => pair.Value))            
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+        };
+
+        var files = Directory.EnumerateFiles(folderPath, "*.txt");
+
+        foreach (var file in files)
+        {
+            var words = tokenizeFile(file);
+            var wordCounts = countWords(words);
+            Console.WriteLine($"Word counts in file {file}:");
+            displayWordCounts(wordCounts);
+            Console.WriteLine();
+        }
     }
 }
